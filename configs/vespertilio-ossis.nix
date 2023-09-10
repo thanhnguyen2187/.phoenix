a5a2248c6f795e94c0b47b3040190df3bb0b5d88
Thanh Nguyen <thanhnguyen2187@gmail.com>
2023-09-10 18:34:01 +0700

    Add config files for `vespertilio`

---
+++ configs/vespertilio-ossis.nix
@@ -0,0 +1,111 @@
+{ config, pkgs, ... }:
+let
+  home-manager = builtins.fetchTarball {
+    url = "https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz";
+    sha256 = "1lhkqlizabh107mgj9b3fsfzz6cwpcmplkwspqqavwqr9dlmlwc4";
+  };
+  nix-alien-pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/thiagokokada/nix-alien/tarball/master";
+    sha256 = "1731kbzrnbr65xwlvkv8r1rilj5ym2yhaxdijwxj552xnda93c4j";
+  }) { };
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
+      (import "${home-manager}/nixos")
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
+  boot.extraModulePackages = [
+    config.boot.kernelPackages.rtl88x2bu
+  ];
+
+  networking.hostName = "nixos"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Asia/Ho_Chi_Minh";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  i18n.inputMethod = {
+    enabled = "ibus";
+    ibus.engines = with pkgs.ibus-engines; [
+      bamboo
+    ];
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.opengl.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.thanh = {
+    isNormalUser = true;
+    description = "Thanh Nguyen";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [
+      nix-alien-pkgs.nix-alien
+    ];
+    shell = pkgs.zsh;
+  };
+  programs.zsh.enable = true;
+
+  virtualisation.docker.enable = true;
+
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+     vim
+     git
+     ibus-engines.bamboo
+  ];
+
+  home-manager.useUserPackages = true;
+  home-manager.useGlobalPkgs = true;
+
+  system.stateVersion = "23.05";
+
+}
---
+++ configs/vespertilio.nix
@@ -0,0 +1,140 @@
+{ config, pkgs, ... }:
+let
+in
+{
+  home.username = "thanh";
+  home.homeDirectory = "/home/thanh";
+
+  home.stateVersion = "23.05";
+
+  programs.home-manager.enable = true;
+
+  # nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfreePredicate = (pkg: true);
+  fonts.fontconfig.enable = true;
+  home.packages = with pkgs; [
+    xclip
+    (nerdfonts.override { fonts = ["JetBrainsMono"]; })
+    neofetch
+    zsh-powerlevel10k
+    mitschemeX11
+    ripgrep
+    hugo
+    go-task
+    texlive.combined.scheme-full
+    nodejs
+    yarn
+    gitui
+    go
+    # elixir
+    # sumneko-lua-language-server
+    # elixir_ls
+    # inotify-tools
+    jq
+    # kubectl
+    # janet
+    # pkgsUnstable.jpm
+    # babashka
+    # clojure
+    jdk
+    tmuxp
+    spotify
+    google-chrome
+    chromedriver
+    peek
+    gerbil
+
+    # tree-sitter
+
+    # cargo
+    # rustc
+    rustup
+    # TODO: find a way to make this feel... less hacky
+    # rust-analyzer
+    # rustfmt
+    # clippy
+    # llvmPackages.bintools # to make `lld` works
+    # (clang.override {ignoreCollisions = true;})
+
+    llvm
+    lld
+    gcc
+    # llvmPackages.libcxxClang
+    # clang
+    pkgconfig
+    # dotnetPackages.Boogie_2_4_1
+    # boogie
+    dotnet-sdk
+
+    calibre
+    sqlitebrowser
+
+    jetbrains.pycharm-community
+    jetbrains.pycharm-professional
+    jetbrains.idea-community
+    jetbrains.webstorm
+    jetbrains.goland
+    jetbrains.datagrip
+
+    python3Full
+    python310Packages.python-lsp-server
+    # pipenv
+
+    gnumake
+    docker-compose
+
+    openssl
+    glib
+    nss
+    nspr
+    xorg.libxcb
+
+    patchelf
+    cmake
+    vivaldi
+    skypeforlinux
+    postgresql
+  ];
+  imports = [
+    ./vim.nix
+    ./zsh.nix
+  ];
+
+  programs.fzf = {
+    enable = true;
+  };
+
+  programs.tmux = {
+    enable = true;
+    # use `gpakosz/.tmux`
+    extraConfig = ''
+      ${builtins.readFile ./tmux/.tmux.conf}
+      ${builtins.readFile ./tmux/.tmux.conf.local}
+    '';
+  };
+
+  programs.git = {
+    enable = true;
+  };
+
+  programs.gh = {
+    enable = true;
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font = {
+        size = 12;
+        normal = {
+          family = "JetBrains Mono Nerd Font";
+          style = "Regular";
+        };
+      };
+    };
+  };
+
+  programs.opam = {
+    enable = true;
+  };
+}
