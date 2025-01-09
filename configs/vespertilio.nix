bb61d292346cde6ccc812b0b8ac2658ce8a153c0
Thanh Nguyen <thanhnguyen2187@gmail.com>
2025-01-09 14:57:53 +0700

    vespertillio: enable `direnv`

--- configs/vespertilio.nix
+++ configs/vespertilio.nix
@@ -138,5 +138,11 @@ in
     enable = true;
   };
 
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true; # see note on other shells below
+    nix-direnv.enable = true;
+  };
+
   home.file.".ideavimrc".text = builtins.readFile ./vim/.ideavimrc;
 }
